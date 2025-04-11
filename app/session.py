6c2dc1854b4e74c9b3f8eaa9f0a690c7bde72688
MohamedRaffik <raffikmohamed7@gmail.com>
2025-04-11 04:17:55 -0400

    fix(session-auth): fix retrieve user handler to instead manually create db sessions instead of using the dependency
    dependencies are not passed to the retrieve user handler, therefore it is not possible to use

--- app/session.py
+++ app/session.py
@@ -1,32 +1,29 @@
 from typing import Any
 
 from litestar.connection import ASGIConnection
-from litestar.di import Provide
 from litestar.middleware.session.server_side import (
     ServerSideSessionBackend,
     ServerSideSessionConfig,
 )
 from litestar.security.session_auth import SessionAuth
 
-from app.db.models.users import User, UserRespository, provide_users_repo
+from app.db import sqlalchemy_config
+from app.db.models.users import User, provide_users_repo
 
 
-async def retrieve_user_handler(
-    session: dict[str, Any],
-    connection: ASGIConnection,
-    users_repo: UserRespository,
-) -> User | None:
-    return (
-        users_repo.get_one_or_none(User.id == user_id)
-        if (user_id := session.get("user_id"))
-        else None
-    )
+async def retrieve_user_handler(session: dict[str, Any], connection: ASGIConnection):
+    async with sqlalchemy_config.get_session() as db_session:
+        users_repo = await provide_users_repo(db_session=db_session)
+        return (
+            await users_repo.get_one_or_none(User.id == user_id)
+            if (user_id := session.get("user_id"))
+            else None
+        )
 
 
 session_auth = SessionAuth[User, ServerSideSessionBackend](
     retrieve_user_handler=retrieve_user_handler,
     session_backend_config=ServerSideSessionConfig(),
-    dependencies={"users_repo": Provide(provide_users_repo)},
     exclude=[
         "/auth/login",
         "/auth/signup",

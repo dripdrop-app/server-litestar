3eebd90ba38be5c09fb3b3f89b88c4394d745295
MohamedRaffik <raffikmohamed7@gmail.com>
2025-04-12 03:21:49 -0400

    refactor(templates): enable async rendering on jinja templates
    adds an engine callback to the template config that modifies the jinja environment instantiated

--- app/templates/__init__.py
+++ app/templates/__init__.py
@@ -3,6 +3,13 @@ from pathlib import Path
 from litestar.contrib.jinja import JinjaTemplateEngine
 from litestar.template.config import TemplateConfig
 
+
+def engine_callback(template_engine: JinjaTemplateEngine):
+    template_engine.engine.is_async = True
+
+
 template_config = TemplateConfig(
-    directory=Path("app/templates"), engine=JinjaTemplateEngine
+    directory=Path("app/templates"),
+    engine=JinjaTemplateEngine,
+    engine_callback=engine_callback,
 )

35d719ece3bed9516690d31049a0f987c4a2e112
MohamedRaffik <raffikmohamed7@gmail.com>
2025-04-13 14:51:38 -0400

    refactor(login): use walrus operator in login endpoint

--- app/controllers/authentication.py
+++ app/controllers/authentication.py
@@ -10,6 +10,7 @@ from litestar.exceptions import (
 )
 from litestar.params import Body
 from litestar_saq.config import TaskQueues
+from redis.asyncio import Redis
 
 from app.db.models.users import User, UserRespository, provide_users_repo
 from app.models.authentication import CreateUser, LoginUser, SessionUser
@@ -38,19 +39,18 @@ class AuthenticationController(Controller):
         users_repo: UserRespository,
         request: Request,
     ) -> dict:
-        existing_user = await users_repo.get_one_or_none(User.email == data.email)
-        if not existing_user:
-            raise NotFoundException()
-        password_verified = bcrypt.checkpw(
-            bytes(data.password, encoding="utf-8"),
-            bytes(existing_user.password, encoding="utf-8"),
-        )
-        if not password_verified:
-            raise NotAuthorizedException(detail="Incorrect Credentials.")
-        if not existing_user.verified:
-            raise NotAuthorizedException(detail="Account is not verified.")
-        request.set_session({"user_id": existing_user.id})
-        return {"detail": "Success."}
+        if existing_user := await users_repo.get_one_or_none(User.email == data.email):
+            password_verified = bcrypt.checkpw(
+                bytes(data.password, encoding="utf-8"),
+                bytes(existing_user.password, encoding="utf-8"),
+            )
+            if not password_verified:
+                raise NotAuthorizedException(detail="Incorrect Credentials.")
+            if not existing_user.verified:
+                raise NotAuthorizedException(detail="Account is not verified.")
+            request.set_session({"user_id": existing_user.id})
+            return {"detail": "Success."}
+        raise NotFoundException()
 
     @get("/logout", status_code=status_codes.HTTP_200_OK)
     async def logout(self, request: Request) -> dict:
@@ -65,8 +65,7 @@ class AuthenticationController(Controller):
         task_queues: TaskQueues,
         request: Request,
     ) -> dict:
-        existing_user = await users_repo.get_one_or_none(User.email == data.email)
-        if existing_user:
+        if await users_repo.get_one_or_none(User.email == data.email):
             raise ClientException(detail="User with this email exists.")
         await users_repo.add(User(email=data.email, password=data.password))
         await enqueue_task(

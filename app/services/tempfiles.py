77601215bab7934e85d44d9955bae1b668d1faca
MohamedRaffik <raffikmohamed7@gmail.com>
2025-04-19 02:10:06 -0400

    refactor(tempfiles): use aiofiles instead of os for managing folder creation
    removes extra code needed to run os operations in a separate thread

---
+++ app/services/tempfiles.py
@@ -0,0 +1,29 @@
+import asyncio
+import shutil
+from pathlib import Path
+
+import aiofiles
+import aiofiles.os
+
+TEMP_DIRECTORY = "temp"
+
+
+async def cleanup_temp_directory():
+    await asyncio.to_thread(shutil.rmtree, TEMP_DIRECTORY)
+
+
+async def create_temp_directory():
+    try:
+        await aiofiles.os.mkdir(TEMP_DIRECTORY)
+    except FileExistsError:
+        pass
+
+
+async def create_new_directory(directory: str):
+    directory_path = Path(TEMP_DIRECTORY).joinpath(Path(directory).resolve())
+    await create_temp_directory()
+    try:
+        await aiofiles.os.mkdir(directory_path)
+    except FileExistsError:
+        pass
+    return directory_path

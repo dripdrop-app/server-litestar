bdf6331e906a0f5a44c6412589c96a077f09cc1d
MohamedRaffik <raffikmohamed7@gmail.com>
2025-04-19 15:03:08 -0400

    refactor(audiotags): move read tags helper function to the AudioTags class

--- app/clients/audiotags.py
+++ app/clients/audiotags.py
@@ -8,7 +8,7 @@ import traceback
 import uuid
 from pathlib import Path
 
-import aiofiles
+import aiofiles.os
 import mutagen.id3
 
 from app.models.music import TagsResponse
@@ -100,35 +100,31 @@ class AudioTags:
         return None
 
     @classmethod
-    def read_tags(cls, file_path: str):
-        return AudioTags(file_path=file_path)
+    async def read_tags(cls, file: bytes, filename: str):
+        TAGS_DIRECTORY = "tags"
 
-
-async def read_tags(file: bytes, filename: str):
-    TAGS_DIRECTORY = "tags"
-
-    tags_directory_path = await tempfiles.create_new_directory(
-        directory=TAGS_DIRECTORY, raise_on_exists=False
-    )
-    directory_id = str(uuid.uuid4())
-    directory_path = Path(tags_directory_path).joinpath(directory_id)
-    await aiofiles.os.mkdir(directory_path)
-    file_path = Path(directory_path).joinpath(filename)
-    tags = TagsResponse()
-    try:
-        async with aiofiles.open(file_path, mode="wb") as f:
-            await f.write(file)
-        audio_tags = await asyncio.to_thread(AudioTags.read_tags, file_path)
-        tags = TagsResponse(
-            title=audio_tags.title,
-            artist=audio_tags.artist,
-            album=audio_tags.album,
-            grouping=audio_tags.grouping,
-            artwork_url=audio_tags.get_artwork_as_base64(),
+        tags_directory_path = await tempfiles.create_new_directory(
+            directory=TAGS_DIRECTORY
         )
-    except Exception:
-        logger.exception(traceback.format_exc())
-    finally:
-        await asyncio.to_thread(shutil.rmtree, directory_path)
+        directory_id = str(uuid.uuid4())
+        directory_path = Path(tags_directory_path).joinpath(directory_id)
+        await aiofiles.os.mkdir(directory_path)
+        file_path = Path(directory_path).joinpath(filename)
+        tags = TagsResponse()
+        try:
+            async with aiofiles.open(file_path, mode="wb") as f:
+                await f.write(file)
+            audio_tags = await asyncio.to_thread(AudioTags, file_path)
+            tags = TagsResponse(
+                title=audio_tags.title,
+                artist=audio_tags.artist,
+                album=audio_tags.album,
+                grouping=audio_tags.grouping,
+                artwork_url=audio_tags.get_artwork_as_base64(),
+            )
+        except Exception:
+            logger.exception(traceback.format_exc())
+        finally:
+            await asyncio.to_thread(shutil.rmtree, directory_path)
 
-    return tags
+        return tags

029bf816dfbe39f1b26211b427a47a0baaad8c2f
MohamedRaffik <raffikmohamed7@gmail.com>
2025-05-16 22:53:16 -0400

    feat(music): add new method to music job model to handle file uploads

--- app/db/models/musicjob.py
+++ app/db/models/musicjob.py
@@ -1,12 +1,16 @@
+import base64
 from datetime import datetime
 
+import httpx
+from litestar.datastructures import UploadFile
 from litestar.plugins.sqlalchemy import base, repository
 from sqlalchemy import TIMESTAMP, ForeignKey
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
-from app.clients import s3
+from app.clients import imagedownloader, s3
 from app.db.models.users import User
+from app.settings import settings
 
 
 class MusicJob(base.UUIDAuditBase):
@@ -40,6 +44,49 @@ class MusicJob(base.UUIDAuditBase):
             await s3.delete_file(filename=self.download_filename)
         if self.original_filename:
             await s3.delete_file(filename=self.original_filename)
+
+    async def _upload_audio_file(self, file: UploadFile):
+        filename = f"{settings.aws_s3_music_folder}/{self.id}/old/{file.filename}"
+        url = s3.resolve_url(filename=filename)
+        await s3.upload_file(
+            filename=filename, body=await file.read(), content_type=file.content_type
+        )
+        self.original_filename = filename
+        self.filename_url = url
+
+    async def _upload_artwork_url(self, artwork_url: str):
+        base64_data = None
+        if artwork_url.startswith(("/9j", "iVBORw0KGgo")):
+            base64_data = artwork_url
+        elif (parts := artwork_url.split("base64,")) and len(parts) == 2:
+            base64_data = parts[-1]
+
+        if base64_data:
+            extension = artwork_url.split(":")[0].split("/")[1]
+            data_string = ",".join(artwork_url.split(",")[1:])
+            data = base64.b64decode(data_string.encode())
+            filename = f"{settings.aws_s3_artwork_folder}/{self.id}/artwork.{extension}"
+            url = s3.resolve_url(filename=filename)
+            await s3.upload_file(
+                filename=filename, body=data, content_type=f"image/{extension}"
+            )
+            self.artwork_url = url
+            self.artwork_filename = filename
+        else:
+            async with httpx.AsyncClient() as client:
+                response = await client.get(artwork_url)
+                if response.is_success and imagedownloader.is_image_link(response):
+                    self.artwork_url = artwork_url
+
+    async def upload_files(
+        self, db_session: AsyncSession, file: UploadFile, artwork_url: str | None = None
+    ):
+        if file:
+            await self._upload_audio_file(file=file)
+        if artwork_url:
+            await self._upload_artwork_url(artwork_url=artwork_url)
+        music_job_repo = MusicJobRespository(session=db_session)
+        await music_job_repo.update(self)
 
 
 class MusicJobRespository(repository.SQLAlchemyAsyncRepository[MusicJob]):

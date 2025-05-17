b41fbae505e0956dae26f072d2b967d946a50f28
MohamedRaffik <raffikmohamed7@gmail.com>
2025-05-16 22:47:02 -0400

    refactor(channels): use memory channels backend instead of redis when testing
    using redis pub sub caused async event loops issues with the test client

--- app/channels.py
+++ app/channels.py
@@ -1,10 +1,23 @@
+from litestar.channels.backends.memory import MemoryChannelsBackend
 from litestar.channels.backends.redis import RedisChannelsPubSubBackend
 from redis.asyncio import Redis
 
-from app.settings import settings
+from app.settings import ENV, settings
 
 MUSIC_JOB_UPDATE = "MUSIC_JOB_UPDATE"
 YOUTUBE_CHANNEL_UPDATE = "YOUTUBE_CHANNEL_UPDATE"
 
+redis_channels_backend = RedisChannelsPubSubBackend(
+    redis=Redis.from_url(settings.redis_url)
+)
+memory_channels_backend = MemoryChannelsBackend()
 
-channels_backend = RedisChannelsPubSubBackend(redis=Redis.from_url(settings.redis_url))
+channels_backend = (
+    redis_channels_backend if settings.env != ENV.TESTING else memory_channels_backend
+)
+
+
+async def publish_message(channel: str, message: str):
+    await channels_backend.on_startup()
+    await channels_backend.publish(data=message, channels=[channel])
+    await channels_backend.on_shutdown()

5612edf16567279b96101abdd9deffe8953c14c8
MohamedRaffik <raffikmohamed7@gmail.com>
2025-06-15 20:58:01 -0400

    refactor(pubsub): update pubsub methods to instantiate redis instances instead of doing so in it's constructor

--- app/pubsub.py
+++ app/pubsub.py
@@ -1,4 +1,5 @@
 import asyncio
+from contextlib import asynccontextmanager
 
 from redis.asyncio import Redis
 
@@ -11,33 +12,46 @@ class PubSub:
         YOUTUBE_CHANNEL_UPDATE = "YOUTUBE_CHANNEL_UPDATE"
 
     def __init__(self, channels: list[str]):
-        self.redis = Redis.from_url(settings.redis_url)
-        self.pubsub = self.redis.pubsub()
         self.channels = channels
 
+    @asynccontextmanager
+    async def _get_redis(self):
+        redis = Redis.from_url(settings.redis_url)
+        try:
+            yield redis
+        finally:
+            await redis.aclose()
+
     async def listen(self, ignore_subscribe_messages=False, timeout=60):
-        await self.pubsub.subscribe(*self.channels)
-        self._listen = True
-        while self._listen:
-            message = await self.pubsub.get_message(
-                ignore_subscribe_messages=False, timeout=timeout
-            )
-            if ignore_subscribe_messages and message and message["type"] == "subscribe":
-                continue
-            yield message
-        await self.pubsub.unsubscribe()
+        async with self._get_redis() as redis:
+            pubsub = redis.pubsub()
+            await pubsub.subscribe(*self.channels)
+            self._listen = True
+            while self._listen:
+                message = await pubsub.get_message(
+                    ignore_subscribe_messages=False, timeout=timeout
+                )
+                if (
+                    ignore_subscribe_messages
+                    and message
+                    and message["type"] == "subscribe"
+                ):
+                    continue
+                yield message
+            await pubsub.unsubscribe()
 
     def stop_listening(self):
         self._listen = False
 
     async def publish_message(self, message: str):
-        encoded_message = message.encode()
-        await asyncio.gather(
-            *[
-                self.redis.publish(channel=channel, message=encoded_message)
-                for channel in self.channels
-            ]
-        )
+        async with self._get_redis() as redis:
+            encoded_message = message.encode()
+            await asyncio.gather(
+                *[
+                    redis.publish(channel=channel, message=encoded_message)
+                    for channel in self.channels
+                ]
+            )
 
     async def close(self):
         await self.close()
